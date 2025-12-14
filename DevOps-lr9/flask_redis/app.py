d0eae830cda25af771a88695e525ae36a365ca38
FanatZorina <96425288+FanatZorina@users.noreply.github.com>
2025-12-14 16:20:08 +0500

    Add files via upload

---
+++ DevOps-lr9/flask_redis/app.py
@@ -0,0 +1,16 @@
+import time
+import redis
+import socket
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    return cache.incr('hits')
+
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello World v.5! I have been seen {} times. My name is: {}'.format(count, socket.gethostname())
